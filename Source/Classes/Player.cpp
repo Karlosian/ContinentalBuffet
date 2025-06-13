ccda00a45eaea3dac92e4afb1c0455b954e11a88
Karlosian <kjmiles360@gmail.com>
2025-06-13 00:10:37 -0400

    made bubble game funcitonal with back button and end screen

--- Source/Classes/Player.cpp
+++ Source/Classes/Player.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 
 #include "Player.h"
 
--- Source/Scenes/BubbleGame.h
+++ Source/Scenes/BubbleGame.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "axmol.h"
+#include "FairyGUI.h"
+#include "GComponent.h"
+#include "GRoot.h"
 
 class BubbleGame : public ax::Scene
 {
@@ -11,10 +14,13 @@ class BubbleGame : public ax::Scene
     void updateScoreLabel();
     void endGame();
     void spawnBubble();
+    fairygui::GRoot* root;
     int score = 0;
     int timeLeft = 30;
-    ax::Label* scoreLabel = nullptr;
-    ax::Label* timerLabel = nullptr;
+    ax::Label* scoreLabel;
+    ax::Label* timerLabel;
+    //load at scene creation to avoid lagspikes
+    fairygui::GComponent* comp;
 public:
     static BubbleGame* createScene();
     BubbleGame();              
